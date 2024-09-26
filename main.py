2a9769726b83d3e02fee65ce1e680206dd969ec4
saldick51 <sal912@yandex.ru>
2024-09-26 10:33:16 +0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+module_1_4
+my_string=input('название вашего предприятия:')
+print(len(my_string))
+my_string="nerpa srz"
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
